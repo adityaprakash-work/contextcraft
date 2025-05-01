dbd4f4bba7ae75c0b7288609a9743c978781996f
adityaprakash-work <adityaprakash.work@gmail.com>
2025-05-02 00:09:49 +0530

    ✨ Added config

--- src/contextcraft/__init__.py
+++ src/contextcraft/__init__.py
@@ -4,9 +4,11 @@ Advanced context comprehension and manipulation for LLMs.
 """
 
 from . import config, lms, programs
+from .config import Config
 
 __all__ = [
     "config",
     "lms",
     "programs",
+    "Config",
 ]
